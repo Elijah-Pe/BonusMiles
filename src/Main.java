7da14cdb9263509674f1d7c170356fd767c81089
Ilya <ilia.petro2014@yandex.ru>
2024-10-21 14:53:44 +0400

    Add main programm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = 12_775;
+        int exchangeMiles = 20;
+
+        int bonusMiles = price / exchangeMiles;
+
+        System.out.println("Цена билета: " + price + " рублей");
+        System.out.println("Одна миля начисляется за " + exchangeMiles + " рублей");
+        System.out.println("Количество начисленных миль: " + bonusMiles);
     }
 }
